aaa94444e2daad92751bfe24352d01d739506ece
Alejandro CR <alejandrocr.1024@gmail.com>
2021-03-22 21:31:38 -0500

    .bat file for download example and explicit download link in the page

---
+++ download/browser.bat
@@ -0,0 +1,1 @@
+start http://localhost/http-tutorial/download/download.php
--- download/download.php
+++ download/download.php
@@ -1,6 +1,6 @@
 <?php
 
-// REQUEST A DOWNLOAD?
+// IS REQUESTING A DOWNLOAD?
 $isDownload = isset($_REQUEST['download']);
 
 if ($isDownload) {
@@ -8,9 +8,20 @@ if ($isDownload) {
     header('Content-Disposition: attachment; filename=file.txt');
     // ANY OUTPUT WILL BE DOWNLOADED
     echo 'hello world';
-} else {
-    // INLINE IS FOR SHOW IN BROWSER
-    header('Content-Disposition: inline');
-    // ANY OUTPUT WILL BE SHOWED
-    echo 'hello world';
+    exit;
 }
+
+// INLINE IS FOR SHOW IN BROWSER
+header('Content-Disposition: inline');
+
+// ANY OUTPUT WILL BE SHOWED
+echo 'hello world';
+
+// YOU CAN USE THE "DOWNLOAD" ATTRIBUTE TO SPECIFY IN THE BROWSER
+echo "<a
+        download
+        style=\"display: block;\"
+        target=\"_blank\"
+        href=\"http://localhost/http-tutorial/download/download.php?download\">
+        Download as file
+    </a>";
